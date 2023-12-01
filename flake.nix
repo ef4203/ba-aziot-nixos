8344565e15152b4b7a79fb15c38b02261ba4c123
Niklas Gollenstede <n.gollenstede@web.de>
2023-12-01 12:31:09 +0100

    fix broken input

--- flake.nix
+++ flake.nix
@@ -10,12 +10,9 @@
   installer = { url = "github:NiklasGollenstede/nixos-installer"; inputs.nixpkgs.follows = "nixpkgs"; inputs.functions.follows = "functions"; };
   systems.url = "github:nix-systems/default-linux";
 
-}; outputs = inputs@{ self, ... }: inputs.functions.lib.importRepo inputs ./. (repo@{ overlays, ... }: let
+}; outputs = inputs@{ self, ... }: inputs.functions.lib.importRepo inputs ./. (repo@{ ... }: let
   lib = repo.lib.__internal__;
 in [ # Run »nix flake show --allow-import-from-derivation« to see what this merges to:
-  repo # lib.* nixosModules.* overlays.*
-
-  (lib.inst.mkSystemsFlake { inherit inputs; }) # nixosConfigurations.* apps.*-linux.* devShells.*-linux.* packages.*-linux.all-systems
-  { packages = lib.fun.packagesFromOverlay { inherit inputs; }; } # packages.*.*
-  { patches = (lib.fun.importWrapped inputs "${self}/patches").result; } # patches.*
+  repo # lib.* nixosModules.* overlays.* packages.*.*  patches.*
+  (inputs.installer.lib.mkSystemsFlake { inherit inputs; }) # nixosConfigurations.* apps.*-linux.* devShells.*-linux.* packages.*-linux.all-systems
 ]); }
